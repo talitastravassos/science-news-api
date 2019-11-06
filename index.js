a6d159d0fda662722a7bcf55e1032219ff433f2c
talitast <talitatravassos.tt@gmail.com>
2019-11-06 12:10:46 -0300

    news to object ok

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
- 
+const express = require("express");
+const request = require("request");
+const cheerio = require("cheerio");
+const app = express();
 
-app.get('/api/news', (req, res) => {
-    res.send([1, 2, 3])
-})
+getNews = () => {
+  let news = [{'jesus': 'cristo'}];
 
-app.listen(4000, () => console.log("listening on port 4000"))
+  request(
+    "https://www.sciencenews.org/all-stories",
+    (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+
+        // const siteHeading = $(".river-with-sidebar__list___1EfmS");
+        // console.log(siteHeading.text())
+
+        // const output = siteHeading.children("li").text();
+        // console.log(output)
+        let posts = []
+
+        $(".river-with-sidebar__list___1EfmS li").each((i, el) => {
+          let post = {};
+          post.title = $(el)
+            .find(".post-item-river__title___J3spU")
+            .text()
+            .replace(/\s\s+/g, "");
+          post.category = $(el)
+            .find(
+              ".post-item-river__content___2Ae_0 .post-item-river__eyebrow___33ASW"
+            )
+            .text()
+            .replace(/\s\s+/g, "");
+          post.summary = $(el)
+            .find(".post-item-river__excerpt___3ok6B")
+            .text()
+            .replace(/\s\s+/g, "");
+          post.author = $(el)
+            .find(".post-item-river__meta___RSvvi span")
+            .text()
+            .replace(/\s\s+/g, "");
+          post.time = $(el)
+            .find(".post-item-river__meta___RSvvi time")
+            .attr().datetime;
+          post.image = $(el)
+            .find("figure a img")
+            .attr().src;
+
+          posts.push(post)  
+        //   console.log(post);
+        //   return post;
+        });
+
+        console.log(posts)
+      }
+    }
+  );
+
+//   console.log(news);
+  return news
+};
+
+app.get("/", (req, res) => {
+  res.send(getNews());
+});
+
+app.get("/api/news", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.listen(4000, () => console.log("listening on port 4000"));
