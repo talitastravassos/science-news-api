6dce1a11c73d52bbb2302be0ec2982a5dbf157ca
talitast <talitatravassos.tt@gmail.com>
2019-11-06 17:26:34 -0300

    create scraping module

--- index.js
+++ index.js
@@ -1,74 +1,34 @@
 const express = require("express");
-const cheerio = require("cheerio");
-const axios = require("axios");
 const app = express();
+const scrap = require('./modules/scraping')
 
-let news = [];
-
-getNews = url => {
-  axios.get(url).then(response => {
-    const $ = cheerio.load(response.data);
-
-    let posts = [];
-
-    $(".river-with-sidebar__list___1EfmS li").each((i, el) => {
-      let post = {};
-      post.id = i + 1
-      post.title = $(el)
-        .find(".post-item-river__title___J3spU")
-        .text()
-        .replace(/\s\s+/g, "");
-      post.category = $(el)
-        .find(
-          ".post-item-river__content___2Ae_0 .post-item-river__eyebrow___33ASW"
-        )
-        .text()
-        .replace(/\s\s+/g, "");
-      post.summary = $(el)
-        .find(".post-item-river__excerpt___3ok6B")
-        .text()
-        .replace(/\s\s+/g, "");
-      post.author = $(el)
-        .find(".post-item-river__meta___RSvvi span")
-        .text()
-        .replace(/\s\s+/g, "");
-      post.datetime = $(el)
-        .find(".post-item-river__meta___RSvvi time")
-        .attr().datetime;
-      post.image = $(el)
-        .find("figure a img")
-        .attr().src;
-      post.url = $(el)
-        .find(".post-item-river__title___J3spU a")
-        .attr().href;
-
-      posts.push(post);
-      pushNewsToArray(posts);
-    });
-  });
-};
-
-pushNewsToArray = data => {
-  news = data;
-  console.log(news);
-};
+const { news, scraping } = scrap
 
 app.get("/", (req, res) => {
-  getNews("https://www.sciencenews.org/all-stories");
+  news.page = 1;
+  scraping("https://www.sciencenews.org/all-stories");
+
   res.send(news);
 });
 
 app.get("/api/news", (req, res) => {
-  getNews("https://www.sciencenews.org/all-stories");
+  news.page = 1;
+  scraping("https://www.sciencenews.org/all-stories");
 
-  res.send(news);
+  setTimeout(() => {
+    res.send(news);
+  }, 5000);
 });
 
 app.get("/api/news/:page", (req, res) => {
-  getNews(`https://www.sciencenews.org/all-stories/page/${parseInt(req.params.page)}`);
+  news.page = parseInt(req.params.page);
+  scraping(
+    `https://www.sciencenews.org/all-stories/page/${parseInt(req.params.page)}`
+  );
 
-
-  res.send(news);
+  setTimeout(() => {
+    res.send(news);
+  }, 5000);
 });
 
 app.listen(4000, () => console.log("listening on port 4000"));
---
+++ modules/scraping.js
@@ -0,0 +1,56 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+let news = {};
+
+//scraping page script
+function scraping(url) {
+  axios.get(url).then(response => {
+    const $ = cheerio.load(response.data);
+
+    let posts = [];
+    // post from web to object
+    $(".river-with-sidebar__list___1EfmS li").each((i, el) => {
+      let post = {};
+      post.id = i + 1;
+      post.title = $(el)
+        .find(".post-item-river__title___J3spU")
+        .text()
+        .replace(/\s\s+/g, "");
+      post.category = $(el)
+        .find(
+          ".post-item-river__content___2Ae_0 .post-item-river__eyebrow___33ASW"
+        )
+        .text()
+        .replace(/\s\s+/g, "");
+      post.summary = $(el)
+        .find(".post-item-river__excerpt___3ok6B")
+        .text()
+        .replace(/\s\s+/g, "");
+      post.author = $(el)
+        .find(".post-item-river__meta___RSvvi span")
+        .text()
+        .replace(/\s\s+/g, "");
+      post.datetime = $(el)
+        .find(".post-item-river__meta___RSvvi time")
+        .attr().datetime;
+      post.image = $(el)
+        .find("figure a img")
+        .attr().src;
+      post.url = $(el)
+        .find(".post-item-river__title___J3spU a")
+        .attr().href;
+
+      posts.push(post);
+    });
+    pushNewsToArray(posts);
+  });
+}
+
+function pushNewsToArray(data) {
+  news.data = data;
+//   console.log(news);
+}
+
+module.exports.scraping = scraping;
+module.exports.news = news;
