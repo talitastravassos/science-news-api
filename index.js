8b21ca87c88dd18ec7160fce3310a8b18e0613ec
talitast <talitatravassos.tt@gmail.com>
2019-11-07 11:51:19 -0300

    fix

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express();
 const routes = require('./routes');
 
 app.use('/api', routes); //register the routes
+app.use(express.static('public'))
 
 app.listen(4000, () => console.log("listening on port 4000"));
